d5a6fbc516cf71b1ce245cf99923a857613d967e
RyanYans <ryanyans32@gmail.com>
2016-11-25 16:14:53 +0800

    - add rNewsDetailActivity

---
+++ app/src/main/java/com/rya/life4beijing/activity/NewsDetailActicity.java
@@ -0,0 +1,21 @@
+package com.rya.life4beijing.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rya.life4beijing.R;
+
+/**
+ * Created by Rya32 on 广东石油化工学院.
+ * Version 1.0
+ */
+
+public class NewsDetailActicity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+
+
+    }
+}
